4a471bb5c819294beda168527c441ff9d5960a3e
danhammer <dan.s.hammer@gmail.com>
2020-08-23 16:36:01 -0700

    added carbon

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import datetime
 from statsmodels.tsa.stattools import adfuller
 import pydeck as pdk
 import plotly.graph_objects as go
+import plotly.express as px
 
 
 st.header("Prioritizing concessions based on Earth observation")
@@ -30,14 +31,16 @@ def load_data(plot=True):
 	water = pd.read_pickle('data/waterclass.pkl')
 	evapo = pd.read_pickle('data/evapotranspiration.pkl')
 	pop = pd.read_pickle('data/population.pkl')
+	soil = pd.read_pickle('data/soil.pkl')
+	carbon = pd.read_pickle('data/carbon.pkl')
 
 	fires = pd.read_pickle('data/fires.pkl')
 	fires.columns = ['fires', 'date', 'block']
 
-	return properties, vi, defor, water, evapo, fires, pop
+	return properties, vi, defor, water, evapo, fires, pop, soil, carbon
 
 
-properties, vi, defor, water, evapo, fires, pop = load_data()
+properties, vi, defor, water, evapo, fires, pop, soil, carbon = load_data()
 
 
 st.markdown("""
@@ -97,10 +100,9 @@ st.markdown("""
 
 	According to the [Facebook population density
 	maps](https://dataforgood.fb.com/tools/population-density-maps/), using
-	building counts derived from satellite imagery, there are **%s** people
-	residing (at least temporarily) within the boundaries of the concession.
-
-	------
+	building counts derived from high-resolution satellite imagery, there are
+	**%s** people residing (at least temporarily) within the boundaries of the
+	concession.
 
 """ % (
 		block_name, 
@@ -113,7 +115,261 @@ st.markdown("""
 	)
 )
 
+carbon_df = carbon[carbon["block"] == block_name]
+
+carbon_df["tC"] = carbon_df["carbon"] * 9 * carbon_df["frequency"]
+
+mtC = np.round(sum(carbon_df["tC"])/1000000, 2)
+
+avgtC_per_hectare = sum(carbon_df["tC"])/sum(carbon_df["frequency"])
+
 st.markdown("""
+
+	----
+
+	## Carbon
+
+	Carbon storage is one of the few ecosystem services with a market value. This
+	section reports the above- and below-ground carbon, as estimated from global
+	models that rely on remotely sensed sources of information.  These are very
+	rough estimates.  A more precise estimate at higher resolution would be
+	required to facilitate carbon market transactions.  
+
+	The total carbon within the **%s** concession, both above- and below-ground,
+	is estimated to be **%s** million tonnes of Carbon (an average of **%s**
+	tC/ha).
+
+""" % (
+		block_name,
+		mtC,
+		np.round(avgtC_per_hectare/9, 2)
+	)
+)
+
+rule = alt.Chart(carbon_df).mark_rule(color='red').encode(
+    x='mean(avgtc):N'
+)
+
+c = alt.Chart(carbon_df).mark_bar(
+		color="#A9BEBE",
+		size=2.5
+	).encode(
+	    x=alt.X(
+	    	'carbon:Q',
+        	scale=alt.Scale(domain=[0,150]),
+        	title="Tonnes (t) of Carbon per hectare (ha)"
+        ),
+	    y=alt.Y(
+	    	'frequency:Q',
+	    	title="Number of pixels (300m)"
+	    )
+)
+
+st.altair_chart(c, use_container_width=True)
+
+
+soil_df = soil[soil["block"] == block_name]
+
+
+
+
+st.markdown(""" 
+
+	### Soil organic carbon density
+
+	### Forest carbon
+
+""")
+
+
+
+# c = alt.Chart(soil_df).transform_fold(
+# 	['b0', 'b10', 'b30', 'b60', 'b100', 'b200']
+# ).mark_area().encode(
+#     alt.X('carbon_g_per_kg:Q'),
+#     alt.Y('frequency'),
+#     alt.Row('depth:N')
+# )
+
+# st.altair_chart(c, use_container_width=True)
+
+st.markdown("""
+
+	----
+
+	## Fires
+
+	The [Fire Information for Resource Management
+	System](https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms)
+	(FIRMS) was developed to provide near real-time active fire locations to
+	natural resource managers that faced challenges obtaining timely
+	satellite-derived fire information.  We report the fires for each day since
+	January 1, 2001 above an intensity threshold of 300 Kelvin.  
+
+""")
+
+fires_df = fires[fires["block"]==block_name]
+
+c = alt.Chart(fires_df).mark_bar(
+		color="#e45756",
+		size=0.6
+	).encode(
+	    x='date:T',
+	    y='fires'
+)
+
+st.altair_chart(c, use_container_width=True)
+
+st.markdown("""
+
+	### Fire anomalies
+
+	There is clearly a seasonal pattern of fires.  How does this year (in red)
+	compare against previous years?  In other words, is the pattern of fires seen
+	this year an anomaly?  The x-axis is _day of year_.  If the red line strays
+	from the grey area, which represents the 95 percent confidence interval based
+	on the previous 11 years, then that day's fire count is considered anomalous.
+	 The bottomline for the initial set of concessions is that the fire count is
+	down this year, relative to the previous decade.
+
+""")
+
+fires_df['year'] = pd.DatetimeIndex(fires_df['date']).year
+fires_df['day_of_year'] = pd.DatetimeIndex(fires_df['date']).dayofyear
+fires_df_early = fires_df[fires_df["year"] < 2020]
+fires_df_late = fires_df[fires_df["year"] == 2020]
+
+fires_ci = alt.Chart(fires_df_early).mark_errorband(extent='ci').encode(
+	    x='day_of_year',
+	    y='fires'
+)
+
+
+fires_smooth = alt.Chart(fires_df_late).mark_line(
+	color='#e45756'
+).transform_window(
+	rolling_mean='mean(fires)',
+	frame=[-10, 10]
+).encode(
+	x=alt.X(
+		'day_of_year',
+		axis=alt.Axis(
+			title=""
+		),
+	),
+	y=alt.Y(
+		'rolling_mean:Q'
+	)
+)
+
+st.altair_chart(fires_ci + fires_smooth, use_container_width=True)
+
+
+
+
+defor_df = defor[defor["block"]==block_name]
+total_defor = sum(defor["hectares"]) 
+total_defor_perc = int(10000 * total_defor/(area * 100))
+total_defor_perc = total_defor_perc/100
+
+st.markdown("""
+
+	----
+
+	## Deforestation 
+
+	Annual tree cover loss between 2000-2019,  detected from NASA satellite
+	imagery at 30-meter resolution.  Defined as a stand-replacement disturbance
+	(achange from a forest to non-forest state).  For **%s**, these numbers
+	suggest that  **%s percent** of the concession was impacted by tree cover
+	loss.
+
+""" %(block_name, total_defor_perc) )
+
+c = alt.Chart(defor_df).mark_bar(
+		color="#e45756"
+	).encode(
+	    x='year:O',
+	    y='hectares'
+)
+
+st.altair_chart(c, use_container_width=True)
+
+water_df = water[water["block"]==block_name]
+total_water_area = np.round(sum(water_df["area_km2"]), 2)
+percent_water_area = np.round(100*total_water_area/area, 2)
+
+st.markdown("""
+
+	----
+
+	## Surface water 
+
+	The Joint Research Centre reports on the location and temporal distribution
+	of surface water from 1984 to 2019 and provides statistics on the extent and
+	change of those water surfaces.  The data are derived from Sentinel satellite
+	imagery, and the methods are published in
+	[_Nature_](https://www.nature.com/articles/nature20584).  The following chart
+	summarizes a 35-year analysis of the JRC output data, representing a
+	comprehensive analysis of surface water and how it has changed over time for
+	the **%s** concession.  Of the %s square kilometers in the concession, **%s**
+	(%s percent) have been, at some point, characterized by surface water or
+	surface water change.  The following chart illustrates the composition of the
+	surface water classes and surface water transitions in the concession. 
+	Hovering over the chart will display the area in square kilometers of each
+	water class/transition.
+
+""" %(
+		block_name, 
+		"{:,d}".format(area), 
+		total_water_area, 
+		percent_water_area
+	)
+)
+
+labels = water_df["water_label"]
+values = water_df["area_km2"]
+
+# Use `hole` to create a donut-like pie chart
+fig = go.Figure(
+	data=[
+		go.Pie(
+				labels=labels, 
+				values=values, 
+				hole=.5,
+				marker=dict(
+					colors=[
+						"rgb(165,0,38)",
+						"rgb(215,48,39)",
+						"rgb(244,109,67)",
+						"rgb(253,174,97)",
+						"rgb(254,224,144)",
+						"rgb(224,243,248)",
+						"rgb(171,217,233)",
+						"rgb(116,173,209)",
+						"rgb(69,117,180)",
+						"rgb(49,54,149)"
+					]
+				)
+			)
+		]
+	)
+
+fig.update_traces(hoverinfo='label+value')
+fig.update_layout(legend=dict(
+    orientation="h",
+    yanchor="bottom",
+    y=1.1,
+    xanchor="right",
+    x=1
+))
+
+st.plotly_chart(fig, use_container_width=True)
+
+
+st.markdown("""
+
+	-------
 
 	## Vegetation 
 
@@ -203,153 +459,6 @@ evapo_smooth = alt.Chart(evapo_df).mark_line(
 st.altair_chart(evapo_raw + evapo_smooth, use_container_width=True)
 
 
-
-defor_df = defor[defor["block"]==block_name]
-total_defor = sum(defor["hectares"]) 
-total_defor_perc = int(10000 * total_defor/(area * 100))
-total_defor_perc = total_defor_perc/100
-
-st.markdown("""
-
-	----
-
-	## Deforestation 
-
-	Annual tree cover loss between 2000-2019,  detected from NASA satellite
-	imagery at 30-meter resolution.  Defined as a stand-replacement disturbance
-	(achange from a forest to non-forest state).  For **%s**, these numbers
-	suggest that  **%s percent** of the concession was impacted by tree cover
-	loss.
-
-""" %(block_name, total_defor_perc) )
-
-c = alt.Chart(defor_df).mark_bar(
-		color="#e45756"
-	).encode(
-	    x='year:O',
-	    y='hectares'
-)
-
-st.altair_chart(c, use_container_width=True)
-
-water_df = water[water["block"]==block_name]
-total_water_area = np.round(sum(water_df["area_km2"]), 2)
-percent_water_area = np.round(100*total_water_area/area, 2)
-
-st.markdown("""
-
-	----
-
-	## Surface water 
-
-	The Joint Research Centre reports on the location and temporal distribution
-	of surface water from 1984 to 2019 and provides statistics on the extent and
-	change of those water surfaces.  The data are derived from Sentinel satellite
-	imagery, and the methods are published in
-	[_Nature_](https://www.nature.com/articles/nature20584).  The following chart
-	summarizes a 35-year analysis of the JRC output data, representing a
-	comprehensive analysis of surface water and how it has changed over time for
-	the **%s** concession.  Of the %s square kilometers in the concession, **%s**
-	(%s percent) have been, at some point, characterized by surface water or
-	surface water change.  The following chart illustrates the composition of the
-	surface water classes and surface water transitions in the concession. 
-	Hovering over the chart will display the area in square kilometers of each
-	water class/transition.
-
-""" %(
-		block_name, 
-		"{:,d}".format(area), 
-		total_water_area, 
-		percent_water_area
-	)
-)
-
-labels = water_df["water_label"]
-values = water_df["area_km2"]
-
-# Use `hole` to create a donut-like pie chart
-fig = go.Figure(
-	data=[
-		go.Pie(
-			labels=labels, 
-			values=values, 
-			hole=.5)
-		]
-	)
-
-fig.update_traces(hoverinfo='label+value')
-
-st.plotly_chart(fig, use_container_width=True)
-
-st.markdown("""
-
-	----
-
-	## Fires
-
-	The [Fire Information for Resource Management
-	System](https://earthdata.nasa.gov/earth-observation-data/near-real-time/firms)
-	(FIRMS) was developed to provide near real-time active fire locations to
-	natural resource managers that faced challenges obtaining timely
-	satellite-derived fire information.  We report the fires for each day since
-	January 1, 2001 above an intensity threshold of 300 Kelvin.  
-
-""")
-
-fires_df = fires[fires["block"]==block_name]
-
-c = alt.Chart(fires_df).mark_bar(
-		color="#e45756",
-		size=0.6
-	).encode(
-	    x='date:T',
-	    y='fires'
-)
-
-st.altair_chart(c, use_container_width=True)
-
-st.markdown("""
-
-	There is clearly a seasonal pattern of fires.  How does this year (in red)
-	compare against previous years?  In other words, is the pattern of fires seen
-	this year an anomaly?  The x-axis is _day of year_.  If the red line strays
-	from the grey area, which represents the 95 percent confidence interval based
-	on the previous 11 years, then that day's fire count is considered anomalous.
-	 The bottomline for the initial set of concessions is that the fire count is
-	down this year, relative to the previous decade.
-
-""")
-
-fires_df['year'] = pd.DatetimeIndex(fires_df['date']).year
-fires_df['day_of_year'] = pd.DatetimeIndex(fires_df['date']).dayofyear
-fires_df_early = fires_df[fires_df["year"] < 2020]
-fires_df_late = fires_df[fires_df["year"] == 2020]
-
-fires_ci = alt.Chart(fires_df_early).mark_errorband(extent='ci').encode(
-	    x='day_of_year',
-	    y='fires'
-)
-
-
-fires_smooth = alt.Chart(fires_df_late).mark_line(
-	color='#e45756'
-).transform_window(
-	rolling_mean='mean(fires)',
-	frame=[-10, 10]
-).encode(
-	x=alt.X(
-		'day_of_year',
-		axis=alt.Axis(
-			title=""
-		),
-	),
-	y=alt.Y(
-		'rolling_mean:Q'
-	)
-)
-
-st.altair_chart(fires_ci + fires_smooth, use_container_width=True)
-
 st.markdown("""
 
 	----
@@ -369,13 +478,12 @@ st.markdown("""
 	studies to demonstrate the [efficacy of multiple use protected
 	areas](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0022722).
 
-	3. Human modifcation (histogram for 2019)
+	3. **Higher spatial and temporal resolution**.  Some measures are daily,
+	through yesterday.  Some are annual.  For those, more up-to-date measures can
+	be derived, but that would require a lot more work.
 
-	4. Land cover
-
-	5. **Higher spatial and temporal resolution**.  Some measures are daily, through yesterday.  Some are annual.  For those,
-	more up-to-date measures can be derived, but that would require a lot more
-	work.
+	4. **Additional datasets**.  Including building outlines and other
+	infrastructure, as well as land use.
 
 """)
 
